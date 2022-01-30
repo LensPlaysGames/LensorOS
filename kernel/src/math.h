f4a9384c4bf16c279b12d7f08552ef943591893a
LensPlaysGames <lensplaysgames@gmail.com>
2022-01-30 13:09:44 -0800

    [MATH]: Rename `Vector2` to more aptly named `uVector2`
    Unsigned Vector2 and Signed Vector2 are very different.

--- kernel/src/math.h
+++ kernel/src/math.h
@@ -1,24 +1,25 @@
 #ifndef LENSOR_OS_MATH_H
 #define LENSOR_OS_MATH_H
 
-struct Vector2 {
+/// Vector with a fixed width of two unsigned 64-bit integer numbers.
+struct uVector2 {
 	u64 x;
 	u64 y;
 
-	Vector2() {
+	uVector2() {
 		x = 0;
 		y = 0;
 	}
 
-	Vector2(u64 _x, u64 _y) {
+	uVector2(u64 _x, u64 _y) {
 		x = _x;
 		y = _y;
 	}
 
-	friend inline bool operator==(const Vector2& lhs, const Vector2& rhs) {
+	friend inline bool operator==(const uVector2& lhs, const uVector2& rhs) {
 		return (lhs.x == rhs.x && lhs.y == rhs.y);
 	}
-	friend inline bool operator!=(const Vector2& lhs, const Vector2& rhs) {
+	friend inline bool operator!=(const uVector2& lhs, const uVector2& rhs) {
 		return !(lhs == rhs);
 	}
 };
