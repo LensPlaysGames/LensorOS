c6fb7d1fd4bbc1aaab54a2b96dc159830a769643
LensPlaysGames <lensplaysgames@gmail.com>
2022-01-31 13:34:09 -0800

    Very small formatting changes

--- kernel/src/pit.cpp
+++ kernel/src/pit.cpp
@@ -9,12 +9,11 @@ void PIT::initialize_pit() {
 	///   Rate Generator
 	///   Binary Coded Decimal Disabled
 	outb(PIT_CMD, 0b00110100);
-	/// Set divisor of channel zero.
+	/// Set divisor of channel zero (system timer).
 	outb(PIT_CH0_DAT, (u8) ((u16)PIT_DIVISOR & 0x00ff));
 	outb(PIT_CH0_DAT, (u8)(((u16)PIT_DIVISOR & 0xff00) >> 8));
 }
-
-
+		
 double PIT::seconds_since_boot() {
 	return (double)Ticks / PIT_FREQUENCY;
 }
