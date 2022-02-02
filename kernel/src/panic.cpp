2870f5d2fc321c72f79dcb4d8937b4bb0c69c40f
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-02 15:04:12 -0800

    [PANIC]: Fix mis-aligned panic message

--- kernel/src/panic.cpp
+++ kernel/src/panic.cpp
@@ -1,11 +1,11 @@
 #include "panic.h"
 
 void panic(const char* panicMessage) {
-	gRend.BackgroundColor = 0xffff0000;
-	gRend.DrawPos = {400, 500};
-	gRend.putstr("LensorOS PANIC MODE");
-	gRend.newl();
-	gRend.newl();
-	gRend.putstr(panicMessage, 0x00000000);
-	gRend.swap();
+    gRend.BackgroundColor = 0xffff0000;
+    gRend.DrawPos = {400, 500};
+    gRend.putstr("LensorOS PANIC MODE");
+    gRend.crlf(400);
+    gRend.crlf(400);
+    gRend.putstr(panicMessage, 0x00000000);
+    gRend.swap();
 }
