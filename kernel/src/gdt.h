7e99b512414b3a3f05f8722d6abb0615ca3b68bf
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-01 20:28:04 -0800

    [GDT]: Untabify
    Also rename `Kernel` and `User` to the proper `Ring0` and `Ring3`

--- kernel/src/gdt.h
+++ kernel/src/gdt.h
@@ -4,27 +4,29 @@
 #include "integers.h"
 
 struct GDTDescriptor {
-	u16 Size;
-	u64 Offset;
+    u16 Size;
+    u64 Offset;
 } __attribute__((packed));
 
 struct GDTEntry {
-	u16 Limit0;
-	u16 Base0;
-	u8 Base1;
-	u8 AccessByte;
-	u8 Limit1_Flags;
-	u8 Base2;
+    u16 Limit0;
+    u16 Base0;
+    u8 Base1;
+    u8 AccessByte;
+    u8 Limit1_Flags;
+    u8 Base2;
 } __attribute__((packed));
 
-// Global Descriptor Table
+/// Global Descriptor Table
+/// The `Code` entry is loaded into CS,
+///   the `Data` entry is loaded into all other segment selector registers.
 struct GDT {
-	GDTEntry Null;
-	GDTEntry KernelCode;
-	GDTEntry KernelData;
-	GDTEntry UserNull;
-	GDTEntry UserCode;
-	GDTEntry UserData;
+    GDTEntry Null;
+    GDTEntry Ring0Code;
+    GDTEntry Ring0Data;
+    GDTEntry Ring3Null;
+    GDTEntry Ring3Code;
+    GDTEntry Ring3Data;
 }__attribute__((packed)) __attribute__((aligned(0x1000)));
 
 extern GDT gGDT;
