7e2be80c4e2d57546ef52c68551acd9693de0545
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-02 12:27:02 -0800

    [FATDriver]: Slight correction in FAT16 get value
    Also position comments relative to referenced code, as well as format the multi-line comments a little better.
    
    If statements with a one-line body have experimentally had their braces removed. I do quite like the look of it; it just cleans it up nicely.

--- kernel/src/FATDriver.cpp
+++ kernel/src/FATDriver.cpp
@@ -21,39 +21,32 @@ void FATDriver::write_from_inode(AHCI::AHCIDriver* ahci, u8 portNumber, Inode* i
 }
 
 u32 FATDriver::get_total_fat_sectors(BootRecord* BR) const {
-    if (BR->BPB.NumSectorsPerFAT == 0) {
+    if (BR->BPB.NumSectorsPerFAT == 0)
         return (*(BootRecordExtension32*)&BR->Extended).NumSectorsPerFAT;
-    }
     return BR->BPB.NumSectorsPerFAT;
 }
 
 u32 FATDriver::get_root_directory_sectors(BootRecord* BR) const {
-    if (BR->BPB.NumSectorsPerFAT == 0) { return 0; }
-    return ((BR->BPB.NumEntriesInRoot * FAT_DIRECTORY_SIZE_BYTES)
-            + (BR->BPB.NumBytesPerSector-1))
-        / BR->BPB.NumBytesPerSector;
+    if (BR->BPB.NumSectorsPerFAT == 0)
+        return 0;
+    return ((BR->BPB.NumEntriesInRoot * FAT_DIRECTORY_SIZE_BYTES) + (BR->BPB.NumBytesPerSector-1)) / BR->BPB.NumBytesPerSector;
 }
 
 u32 FATDriver::get_first_root_directory_sector(BootRecord* BR, FATType type) const {
-    if (type == FATType::FAT12 || type == FATType::FAT16) {
+    if (type == FATType::FAT12 || type == FATType::FAT16)
         return get_first_data_sector(BR) + get_root_directory_sectors(BR);
-    }
-    else if (type == FATType::FAT32 || type == FATType::ExFAT) {
+    else if (type == FATType::FAT32 || type == FATType::ExFAT)
         return get_first_sector_in_cluster(BR, ((BootRecordExtension32*)BR->Extended)->RootCluster);
-    }
-    // FIXME: Should handle this error better.
+    // TODO: Handle invalid type (some sort of ErrorOr<T> template would be nice).
     return -1;
 }
 
 u32 FATDriver::get_root_directory_cluster(BootRecord* BR, FATType type) const {
-    if (type == FATType::FAT12 || type == FATType::FAT16) {
-        return get_cluster_from_first_sector(BR, (get_first_data_sector(BR)
-                                                  - get_root_directory_sectors(BR)));
-    }
-    else if (type == FATType::FAT32 || type == FATType::ExFAT) {
+    if (type == FATType::FAT12 || type == FATType::FAT16)
+        return get_cluster_from_first_sector(BR, (get_first_data_sector(BR) - get_root_directory_sectors(BR)));
+    else if (type == FATType::FAT32 || type == FATType::ExFAT)
         return ((BootRecordExtension32*)BR->Extended)->RootCluster;
-    }
-    // FIXME: Should handle this error better.
+    // TODO: Handle invalid type.
     return -1;
 }
 
@@ -86,18 +79,13 @@ FATType FATDriver::get_type(BootRecord* BR) const {
     // TODO: More fool-proof method of detecting FAT type.
     // Get FAT type based on total cluster amount (mostly correct).
     u32 totalClusters = get_total_clusters(BR);
-    if (totalClusters == 0) {
+    if (totalClusters == 0)
         return FATType::ExFAT;
-    }
-    else if (totalClusters < 4085) {
+    else if (totalClusters < 4085)
         return FATType::FAT12;
-    }
-    else if (totalClusters < 65525) {
+    else if (totalClusters < 65525)
         return FATType::FAT16;
-    }
-    else {
-        return FATType::FAT32;
-    }
+    return FATType::FAT32;
 }
 
 /// Try to parse a FAT boot record from the first boot sector of the SATA device.
@@ -123,18 +111,13 @@ bool FATDriver::is_device_fat_formatted(AHCI::AHCIDriver* ahci, u8 portNumber) {
         // [ ] Root cluster is valid (FAT32)
         // [ ] File system version is zero (FAT32)
         // [x] NumFATsPresent greater than zero
-        if (*(u16*)((u64)ahci->Ports[portNumber]->buffer + 510) != 0xaa55
-            || br->BPB.NumFATsPresent <= 0
-            || get_total_sectors(br) == 0)
-        {
+        bool invalid = (*(u16*)((u64)ahci->Ports[portNumber]->buffer + 510) != 0xaa55
+                        || br->BPB.NumFATsPresent <= 0 || get_total_sectors(br) == 0);
+        if (invalid)
             result = false;
-        }
         delete br;
     }
-    else {
-        // Read failed.
-        result = false;
-    }
+    else result = false;
     return result;
 }
 
@@ -157,13 +140,13 @@ void FATDriver::read_root_dir(AHCI::AHCIDriver* ahci, u8 portNumber, BootRecord*
         ahci->Ports[portNumber]->Read(get_first_sector_in_cluster(BR, clusterIndex),
                                       BR->BPB.NumSectorsPerCluster,
                                       ahci->Ports[portNumber]->buffer);
-        // Parse port buffer cluster entries.
         clEntry = (ClusterEntry*)ahci->Ports[portNumber]->buffer;
         lfnBufferFull = false;
-        // TODO: FIX INFINITE LOOP HERE
         while (true) {
-            if (clEntry->FileName[0] == 0)    { break;    }
-            if (clEntry->FileName[0] == 0xe5) { continue; }
+            if (clEntry->FileName[0] == 0)
+                break;
+            if (clEntry->FileName[0] == 0xe5)
+                continue;
 
             // Detect a long file name entry
             if (clEntry->Attributes == 0x0f) {
@@ -188,23 +171,18 @@ void FATDriver::read_root_dir(AHCI::AHCIDriver* ahci, u8 portNumber, BootRecord*
             /// Directory: 0b00010000
             /// Archive:   0b00100000
             srl.writestr("   ");
-            if (clEntry->Attributes & 0b00000001) {
+            if (clEntry->Attributes & 0b00000001)
                 srl.writestr(" Read-only ");
-            }
-            if (clEntry->Attributes & 0b00000010) {
+            if (clEntry->Attributes & 0b00000010)
                 srl.writestr(" Hidden ");
-            }
-            if (clEntry->Attributes & 0b00000100) {
+            if (clEntry->Attributes & 0b00000100)
                 srl.writestr(" System ");
-            }
-            else { srl.writeb((u8)' '); }
+            else srl.writeb((u8)' ');
             
-            if (clEntry->Attributes & 0b00010000) {
+            if (clEntry->Attributes & 0b00010000)
                 srl.writestr("Directory");
-            }
-            else if (clEntry->Attributes & 0b00001000) {
+            else if (clEntry->Attributes & 0b00001000)
                 srl.writestr("Volume Label");
-            }
             else {
                 is_file = true;
                 srl.writestr("File");
@@ -241,79 +219,76 @@ void FATDriver::read_root_dir(AHCI::AHCIDriver* ahci, u8 portNumber, BootRecord*
         clusterNumber = clEntry->ClusterNumberL;
         clusterNumber |= (u32)clEntry->ClusterNumberH << 16;
 
-        if (type == FATType::FAT12) {
+        if (type == FATType::FAT12)
             FAToffset = clusterNumber + (clusterNumber / 2);
-        }
-        else if (type == FATType::FAT16) {
+        else if (type == FATType::FAT16)
             FAToffset = clusterNumber * 2;
-        }
-        else if (type == FATType::FAT32
-                 || type == FATType::ExFAT)
-        {
+        else if (type == FATType::FAT32 || type == FATType::ExFAT)
             FAToffset = clusterNumber * 4;
-        }
 
         FATsector = BR->BPB.NumReservedSectors + (FAToffset / BR->BPB.NumBytesPerSector);
         entryOffset = FAToffset % BR->BPB.NumBytesPerSector;
+        
         // Entries under index 0 and 1 are reserved.
-        if (entryOffset <= 1) { break; }
-
         // Don't read same file twice in a row.
-        if (FATsector == lastFATsector) { break; }
+        if (entryOffset <= 1 || FATsector == lastFATsector)
+            break;
 
         ahci->Ports[portNumber]->Read(FATsector, 1, (void*)FAT);
         lastFATsector = FATsector;
-        
+
         if (type == FATType::FAT12) {
-            /* If table value is greater or equal to 0x0ff8,
-                 then there are no more clusters in the cluster chain (entire file read).
-               Else if table value is equal to 0x0ff7,
-                 then this cluster is marked as "bad" (prone to errors, should be avoided).
-               Else, table value contains the cluster number of the next cluster in the file. */
             tableValue = *(u32*)((u16*)&FAT[entryOffset]);
-            if (clusterNumber & 0b1) {
+            /* If table value is greater or equal to 0x0ff8,
+             *   then there are no more clusters in the cluster chain (entire file read).
+             * Else if table value is equal to 0x0ff7, then this cluster is marked 
+             *   as "bad", meaning it is prone to errors and should be avoided.
+             * Else, table value contains the cluster number of the next cluster in the file. 
+             */
+            if (clusterNumber & 0b1)
                 tableValue = tableValue >> 4;
-            }
-            else {
-                tableValue = tableValue & 0x0fff;
-            }
-            if      (tableValue == 0x0ff8) { break;    }
-            else if (tableValue == 0x0ff7) { continue; }
+            else tableValue = tableValue & 0x0fff;
+            
+            if (tableValue == 0x0ff8)
+                break;
+            else if (tableValue == 0x0ff7)
+                continue;
         }
         else if (type == FATType::FAT16) {
-            /* If table value is greater or equal to 0xfff8,
-                 then there are no more clusters in the cluster chain (entire file read).
-               Else if table value is equal to 0xfff7,
-                 then this cluster is marked as "bad" (prone to errors, should be avoided).
-               Else, table value contains the cluster number of the next cluster in the file. */
             tableValue = *(u32*)((u16*)&FAT[entryOffset]);
-            if (clusterNumber & 0b1) {
-                tableValue = tableValue >> 4;
-            }
-            else {
-                tableValue = tableValue & 0x0fff;
-            }
-            if      (tableValue == 0xfff8) { break;    }
-            else if (tableValue == 0xfff7) { continue; }
+            /* If table value is greater or equal to 0xfff8,
+             *   then there are no more clusters in the cluster chain (entire file read).
+             * Else if table value is equal to 0xfff7, then this cluster is marked 
+             *   as "bad", meaning it is prone to errors and should be avoided.
+             * Else, table value contains the cluster number of the next cluster in the file. 
+             */
+            if (tableValue == 0xfff8)
+                break;
+            else if (tableValue == 0xfff7)
+                continue;
         }
         else if (type == FATType::FAT32) {
-            /* If table value is greater or equal to 0x0ffffff8,
-                 then there are no more clusters in the cluster chain (entire file read).
-               Else if table value is equal to 0x0ffffff7,
-                 then this cluster is marked as "bad" (prone to errors, should be avoided).
-               Else, table value contains the cluster number of the next cluster in the file. */
             tableValue = *(u32*)&FAT[entryOffset];
             tableValue &= 0x0fffffff;
-            if      (tableValue >= 0x0ffffff8) { break;    }
-            else if (tableValue == 0x0ffffff7) { continue; }
+            /* If table value is greater or equal to 0x0ffffff8,
+             *   then there are no more clusters in the cluster chain (entire file read).
+             * Else if table value is equal to 0x0ffffff7, then this cluster is marked 
+             *   as "bad", meaning it is prone to errors and should be avoided.
+             * Else, table value contains the cluster number of the next cluster in the file. 
+             */
+            if (tableValue >= 0x0ffffff8)
+                break;
+            else if (tableValue == 0x0ffffff7)
+                continue;
         }
         else if (type == FATType::ExFAT) {
-            /* If table value is greater or equal to 0xfffffff8,
-                 then there are no more clusters in the cluster chain (entire file read).
-               Else if table value is equal to 0xfffffff7,
-                 then this cluster is marked as "bad" (prone to errors, should be avoided).
-               Else, table value contains the cluster number of the next cluster in the file. */
             tableValue = *(u32*)&FAT[entryOffset];
+            /* If table value is greater or equal to 0xfffffff8,
+             *   then there are no more clusters in the cluster chain (entire file read).
+             * Else if table value is equal to 0xfffffff7, then this cluster is marked 
+             *   as "bad", meaning it is prone to errors and should be avoided.
+             * Else, table value contains the cluster number of the next cluster in the file. 
+             */
             if      (tableValue >= 0xfffffff8) { break;    }
             else if (tableValue == 0xfffffff7) { continue; }
         }
@@ -321,7 +296,6 @@ void FATDriver::read_root_dir(AHCI::AHCIDriver* ahci, u8 portNumber, BootRecord*
         // Read next cluster based on tableValue giving cluster number
         clusterIndex = tableValue;
     }
-
     delete[] lfnBuffer;
     delete[] FAT;
 }
