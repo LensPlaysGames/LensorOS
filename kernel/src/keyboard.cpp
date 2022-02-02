4513d5d6c52dfe681e855f42bbca2ca84caa47dc
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-02 14:29:12 -0800

    [KB]: Use correct character size based on font rather than guessing `24`.

--- kernel/src/keyboard.cpp
+++ kernel/src/keyboard.cpp
@@ -7,59 +7,60 @@ bool isLSHIFT;
 bool isRSHIFT;
 
 void handle_keyboard(uint8_t scancode) {
-	uVector2 cachedPos = gRend.DrawPos;
-	switch (scancode) {
-	case LSHIFT:
-		isLSHIFT = true;
-		return;
-	case LSHIFT + 0x80:
-		isLSHIFT = false;
-		return;
-	case RSHIFT:
-		isRSHIFT = true;
-		return;
-	case RSHIFT + 0x80:
-		isRSHIFT = false;
-		return;
-	case ENTER:
-		gRend.DrawPos = gTextPosition;
-		gRend.crlf();
-		gTextPosition = gRend.DrawPos;
-		gRend.DrawPos = cachedPos;
-		return;
-	case ENTER + 0x80:
-		return;
-	case BACKSPACE:
-		gRend.DrawPos = gTextPosition;
-	 	gRend.clearchar();
-		gTextPosition = gRend.DrawPos;
-		gRend.swap(gTextPosition, {8, 24});
-		gRend.DrawPos = cachedPos;
-	 	return;
-	case BACKSPACE + 0x80:
-	 	return;
-	case SPACE:
-		gRend.DrawPos = gTextPosition;
-		gRend.putchar(' ');
-		gRend.swap(gTextPosition, {8, 24});
-		gTextPosition = gRend.DrawPos;
-		gRend.DrawPos = cachedPos;
-		return;
-	case SPACE + 0x80:
-		return;
-	case CAPSLOCK:
-		isCAPS = !isCAPS;
-		return;
-	case CAPSLOCK + 0x80:
-		return;
-	}
+    uVector2 CharacterSize = {8, gRend.Font->PSF1_Header->CharacterSize};
+    uVector2 cachedPos = gRend.DrawPos;
+    switch (scancode) {
+    case LSHIFT:
+        isLSHIFT = true;
+        return;
+    case LSHIFT + 0x80:
+        isLSHIFT = false;
+        return;
+    case RSHIFT:
+        isRSHIFT = true;
+        return;
+    case RSHIFT + 0x80:
+        isRSHIFT = false;
+        return;
+    case ENTER:
+        gRend.DrawPos = gTextPosition;
+        gRend.crlf();
+        gTextPosition = gRend.DrawPos;
+        gRend.DrawPos = cachedPos;
+        return;
+    case ENTER + 0x80:
+        return;
+    case BACKSPACE:
+        gRend.DrawPos = gTextPosition;
+        gRend.clearchar();
+        gTextPosition = gRend.DrawPos;
+        gRend.swap(gTextPosition, CharacterSize);
+        gRend.DrawPos = cachedPos;
+        return;
+    case BACKSPACE + 0x80:
+        return;
+    case SPACE:
+        gRend.DrawPos = gTextPosition;
+        gRend.putchar(' ');
+        gRend.swap(gTextPosition, CharacterSize);
+        gTextPosition = gRend.DrawPos;
+        gRend.DrawPos = cachedPos;
+        return;
+    case SPACE + 0x80:
+        return;
+    case CAPSLOCK:
+        isCAPS = !isCAPS;
+        return;
+    case CAPSLOCK + 0x80:
+        return;
+    }
 
-	char ascii = QWERTY::Translate(scancode, isLSHIFT | isRSHIFT | isCAPS);
-	if (ascii != 0) {
-		gRend.DrawPos = gTextPosition;
-		gRend.putchar(ascii);
-		gRend.swap(gTextPosition, {8, 24});
-		gTextPosition = gRend.DrawPos;
-		gRend.DrawPos = cachedPos;
-	}
+    char ascii = QWERTY::Translate(scancode, isLSHIFT | isRSHIFT | isCAPS);
+    if (ascii != 0) {
+        gRend.DrawPos = gTextPosition;
+        gRend.putchar(ascii);
+        gRend.swap(gTextPosition, CharacterSize);
+        gTextPosition = gRend.DrawPos;
+        gRend.DrawPos = cachedPos;
+    }
 }
