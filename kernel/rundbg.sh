cfe9682c3fb7f5dd26295e9d2407246a6fd80390
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-03 14:35:14 -0800

    Fix broken paths in `run(dbg).sh`

--- kernel/rundbg.sh
+++ kernel/rundbg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 OSName="LensorOS"
-BuildDirectory="$0/../bin"
-OVMFDirectory="$0/../../OVMFbin"
+BuildDirectory="bin"
+OVMFDirectory="../OVMFbin"
 qemu-system-x86_64 -s -S -cpu qemu64 -m 100M -rtc base=localtime,clock=host,driftfix=none -machine q35 -serial stdio -vga cirrus -drive format=raw,file=$BuildDirectory/$OSName.img -drive if=pflash,format=raw,unit=0,file="$OVMFDirectory/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="$OVMFDirectory/OVMF_VARS-pure-efi.fd" -net none
