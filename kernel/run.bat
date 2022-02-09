fbc995b153fcaa46c16e9a99061aa69ef81e0d79
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-09 15:42:17 -0800

    Run Scripts: Inform QEMU to dump CPU registers on event of a triple fault
    This makes it easier to debug those  J U I C Y  triple faults.

--- kernel/run.bat
+++ kernel/run.bat
@@ -4,5 +4,5 @@ set OVMFDIR=%0/../../OVMFbin
 
 set BUILDDIR=%BUILDDIR:"=%
 set OVMFDIR=%OVMFDIR:"=%
-qemu-system-x86_64 -cpu qemu64 -m 100M -rtc base=localtime,clock=host,driftfix=none -machine q35 -serial stdio -vga cirrus -drive format=raw,file=%BUILDDIR%/%OSNAME%.img -drive if=pflash,format=raw,unit=0,file=%OVMFDIR%/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=%OVMFDIR%/OVMF_VARS-pure-efi.fd -net none
+qemu-system-x86_64 -cpu qemu64 -m 100M -rtc base=localtime,clock=host,driftfix=none -machine q35 -serial stdio -vga cirrus -d cpu_reset -drive format=raw,file=%BUILDDIR%/%OSNAME%.img -drive if=pflash,format=raw,unit=0,file=%OVMFDIR%/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=%OVMFDIR%/OVMF_VARS-pure-efi.fd -net none
 pause
