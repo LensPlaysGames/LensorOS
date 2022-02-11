34dbcbb7311e38f2861ff83ee7a54b8627a3c4f8
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-11 11:08:57 -0800

    Update io.h
    - Use fixed width unsigned integer aliases. The only reason it didn't is because this was built back in the olden days :^)

--- kernel/src/io.h
+++ kernel/src/io.h
@@ -1,12 +1,12 @@
 #ifndef LENSOR_OS_IO_H
 #define LENSOR_OS_IO_H
 
-#include <stdint.h>
+#include "integers.h"
 #include "cstr.h"
 #include "basic_renderer.h"
 
-void outb(uint16_t port, uint8_t value);
-uint8_t inb(uint16_t port);
+void outb(u16 port, u8 value);
+uint8_t inb(u16 port);
 void io_wait();
 
 #endif
