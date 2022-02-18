aa076ee5fadaa925d7bb7a46525bd5a8f18cbf32
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-17 23:19:07 -0800

    Create smart_pointer.h
    It's a start.

---
+++ kernel/src/smart_pointer.h
@@ -0,0 +1,72 @@
+#ifndef LENSOR_OS_SMART_POINTER_H
+#define LENSOR_OS_SMART_POINTER_H
+
+template <typename T>
+class SmartPtr {
+public:
+    SmartPtr() : ptr(nullptr) {}
+    SmartPtr(T* ptr) : ptr(ptr) {}
+
+    ~SmartPtr() {
+        if (ptr != nullptr)
+            delete ptr;
+    }
+
+    T* get() {
+        return this->ptr;
+    }
+
+    T* operator -> () {
+        return this->ptr;
+    }
+
+    T& operator * () {
+        return *(this->ptr);
+    }
+
+private:
+    T* ptr;
+};
+
+template <typename T>
+class SmartPtr<T[]> {
+public:
+    SmartPtr() : ptr(nullptr) {}
+    SmartPtr(T* ptr, u64 size) : ptr(ptr), size(size) {}
+    SmartPtr(T* ptr, int size) : ptr(ptr), size((u64)size) {}
+
+    ~SmartPtr() {
+        if (ptr != nullptr)
+            delete[] ptr;
+    }
+
+    T* get() {
+        return this->ptr;
+    }
+
+    T* operator -> () {
+        return this->ptr;
+    }
+
+    T& operator * () {
+        return *(this->ptr);
+    }
+
+    T& operator [] (int index) {
+        if (index < 0) {
+            // TODO: Throw exception, or something.
+            return *(this->ptr);
+        }
+        if ((u64)index >= size) {
+            // TODO: Throw exception...
+            return *(this->ptr);
+        }
+        return this->ptr[index];            
+    }
+
+private:
+    T* ptr;
+    u64 size;
+};
+
+#endif /* if not defined LENSOR_OS_SMART_POINTER_H */
