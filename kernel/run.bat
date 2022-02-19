2554057d4ef31dbbec3726c33aaa701fbdfd3c73
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-19 03:29:16 -0800

    OVMF: Use a copy of OVMF_VARS
    This allows it to never show up in the working tree of git, without requiring any workarounds like `--assume-unchanged` (or actual solutions like running `--skip-worktree`).
    
    This way, the modified files won't get tracked (as they don't need to be when modified by QEMU for a local session), but any updates to the default will be updated and used only if the old one is deleted.
    
    Also just going to mention how crazy the if/else syntax is in bash.

--- kernel/run.bat
+++ kernel/run.bat
@@ -1,8 +1,6 @@
-set OSNAME=LensorOS
-set BUILDDIR=%0/../bin
-set OVMFDIR=%0/../../OVMFbin
-
-set BUILDDIR=%BUILDDIR:"=%
-set OVMFDIR=%OVMFDIR:"=%
-qemu-system-x86_64 -cpu qemu64 -m 100M -rtc base=localtime,clock=host,driftfix=none -machine q35 -serial stdio -vga cirrus -d cpu_reset -drive format=raw,file=%BUILDDIR%/%OSNAME%.img -drive if=pflash,format=raw,unit=0,file=%OVMFDIR%/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=%OVMFDIR%/OVMF_VARS-pure-efi.fd -net none
-pause
+SET BuildDirectory=%0/../bin
+SET OVMFbin=%0/../../OVMFbin
+SET BuildDirectory=%BuildDirectory:"=%
+SET OVMFbin=%OVMFbin:"=%
+IF NOT EXIST %OVMFbin%/OVMF_VARS_LensorOS.fd cp %OVMFbin%/OVMF_VARS-pure-efi.fd %OVMFbin%/OVMF_VARS_LensorOS.fd
+qemu-system-x86_64 -cpu qemu64 -m 100M -rtc base=localtime,clock=host,driftfix=none -machine q35 -serial stdio -vga cirrus -d cpu_reset -drive format=raw,file=%BuildDirectory%/LensorOS.img -drive if=pflash,format=raw,unit=0,file=%OVMFbin%/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=%OVMFbin%/OVMF_VARS_LensorOS.fd -net none
--- kernel/run.sh
+++ kernel/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-OSName="LensorOS"
 ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BuildDirectory="$ScriptDirectory/bin"
 OVMFDirectory="$ScriptDirectory/../OVMFbin"
-qemu-system-x86_64 -cpu qemu64 -m 100M -rtc base=localtime,clock=host,driftfix=none -machine q35 -serial stdio -vga cirrus -d cpu_reset -drive format=raw,file=$BuildDirectory/$OSName.img -drive if=pflash,format=raw,unit=0,file=$OVMFDirectory/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=$OVMFDirectory/OVMF_VARS-pure-efi.fd -net none
+if ! [ -f $OVMFDirectory/OVMF_VARS_LensorOS.fd ] ; then
+    cp $OVMFDirectory/OVMF_VARS-pure-efi.fd $OVMFDirectory/OVMF_VARS_LensorOS.fd
+fi
+qemu-system-x86_64 -cpu qemu64 -m 100M -rtc base=localtime,clock=host,driftfix=none -machine q35 -serial stdio -vga cirrus -d cpu_reset -drive format=raw,file=$BuildDirectory/LensorOS.img -drive if=pflash,format=raw,unit=0,file=$OVMFDirectory/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=$OVMFDirectory/OVMF_VARS_LensorOS.fd -net none
