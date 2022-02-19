397dc9ea20a7464f08e2a9466d41ce382bcbee7c
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-19 05:02:45 -0800

    Spinlock: Use compare-and-swap vs test-and-set
    Also improve documentation on both methods.
    
    In the future, different architectures could derive from a base SpinlockLocker class and over-ride these functions to what is supported by the hardware; then a function pointer could be utilized to use any one of the supported functions to lock the lock.

--- kernel/src/spinlock.cpp
+++ kernel/src/spinlock.cpp
@@ -1,18 +1,55 @@
 #include "spinlock.h"
 
-bool Spinlock::test_and_set(bool* pointer_to_lock_flag, bool new_value) {
-    /* In x86, when an instruction is prefixed with `lock`, it is guaranteed to be run atomically (thread-safe).
-     * The `xchg` instruction sets a memory address or register to a new value while returning the old value in RAX.
-     * It can be determined that the lock *was* free (unlocked) and *is* acquired (locked)
-     *   by currently executing code by testing that the old value that is returned is equal to false.
+bool SpinlockLocker::compare_and_swap
+(
+ bool* pointer_to_lock_flag
+ , bool expected_value_before_swap
+ , bool new_value
+ )
+{
+    // It can be determined the lock was free and has been
+    //   acquired by passing an expected value before swap of `false`.
+    /* Inline Assembly:
+     * |- Desc:
+     * |  |- `lock`                  -- Ensures that the following instruction runs atomically.
+     * |  |- `cmpxchg <op0>, <op1>`  -- Compare 'A' register with <op0>; if equal, swap <op1> and <op0>.
+     * |  |- `sete <op0>`            -- Assign <op0> to the 'equal' flag of the CPU (ZF==1).
+     * |- Outputs:
+     * |  |- "=q"  -- Write-only low-byte-accessible register; return value
+     * |  `- "=m"  -- Write-only memory address; lock flag
+     * `- Inputs:
+     *    |- "r"  -- Any register; new value to swap to if expected  -- "%0"
+     *    |- "m"  -- Memory address; lock flag  -- "%1"
+     *    `- "a"  -- The 'A' register; lock flag will be set only if it matches this value  -- "%2"
+     */
+    bool ret;
+    asm volatile("lock cmpxchg %2, %1\n"
+                 "sete %0\n"
+                 : "=q" (ret), "=m" (*pointer_to_lock_flag)
+                 : "r" (new_value), "m" (*pointer_to_lock_flag), "a" (expected_value_before_swap)
+                 : "memory");
+    return ret;
+}
+
+bool SpinlockLocker::test_and_set(bool* pointer_to_lock_flag, bool new_value) {
+    /* In x86, when an instruction is prefixed with `lock`, 
+     *   it is guaranteed to be run atomically (thread-safe).
+     * The `xchg` instruction sets a memory address or register 
+     *   to a new value while returning the old value in RAX.
+     * It can be determined that the lock was free (unlocked) and is 
+     *   acquired (locked) by currently executing code by testing
+     *   that the old value that is returned is equal to false.
      */
     
-     /* Inline Assembly:
-     * |- Input(s)
-     * |  |- "m" -> Memory Address of lock flag
-     * |  `- "r" -> Register of new lock flag value
-     * `- Output(s)
-     *    `- "=a" -> Write-only; output in RAX
+    /* Inline Assembly:
+     * |- Desc: Old value is set to lock, then lock is set to new value.
+     * |  |- `lock`               -- Ensures that the following instruction runs atomically.
+     * |  |- `xchg <op0>, <op1>`  -- Exchanges the contents of the two operands <op1> into <op0>.
+     * |- Output(s)
+     * |  `- "=a"  -- Write-only; output in RAX
+     * `- Input(s)
+     *    |- "m"  -- Memory Address; lock flag
+     *    `- "r"  -- Any register; new lock flag value
      */
     bool old_value;
     asm volatile ("lock xchg %0, %1" : "=a" (old_value) : "m" (pointer_to_lock_flag), "r" (new_value));
@@ -30,7 +67,7 @@ SpinlockLocker::~SpinlockLocker() {
 
 void SpinlockLocker::lock(Spinlock& lock) {
     // Spin CPU until atomic exchange returns false, meaning lock has been acquired.
-    while (lock.test_and_set(&lock.locked, true));
+    while (compare_and_swap(&lock.locked, false, true) == true);
 }
 
 void SpinlockLocker::unlock() {
--- kernel/src/spinlock.h
+++ kernel/src/spinlock.h
@@ -13,8 +13,6 @@ public:
 
 private:
     bool locked { false };
-    // Also known as "atomic exchange"
-    bool test_and_set(bool* pointer_to_lock_flag, bool new_value);
 };
 
 class SpinlockLocker {
@@ -28,6 +26,31 @@ private:
     Spinlock Lock;
 
     inline void lock(Spinlock&);
+
+    /* Compare and Swap
+     *   If value at pointer is equal to expected value, 
+     *     update value at pointer to new value.
+     *
+     *   This can be done atomically using the x86
+     *     `cmpxchg` instruction with a `lock` prefix.
+     *
+     *   Returns `true` when lock has been acquired given
+     *     expected_value_before_swap = `false`, `new_value` = `true`,
+     *     and pointer_to_lock_flag points to `Lock.locked`.
+     */
+    inline bool compare_and_swap(bool* pointer_to_lock_flag, bool expected_value_before_swap, bool new_value);
+
+    /* Test and Set
+     *   Cache original value of lock, then set the value to the new value.
+     *   If the old value is equal to `false`, the lock has been acquired.
+     *
+     *   This can be done atomically using the x86
+     *     `xchg` instruction with a `lock` prefix.
+     *
+     *   Returns `false` when lock has been acquired given
+     *     new_value = `true` and pointer_to_lock_flag points to `Lock.locked`.
+     */
+    inline bool test_and_set(bool* pointer_to_lock_flag, bool new_value);
 };
 
 #endif /* if !defined LENSOR_OS_SPIN_LOCK_H */
