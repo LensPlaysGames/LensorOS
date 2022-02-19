55df4445a612cffa84ccbc96e5a825d98656de7e
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-19 02:03:57 -0800

    Spinlock: Clarification comments
    Sometimes inline assembly can be really bad to come back to after a while, so I made it a little easier to digest by including as much information as possible.

--- kernel/src/spinlock.cpp
+++ kernel/src/spinlock.cpp
@@ -1,6 +1,19 @@
 #include "spinlock.h"
 
 bool Spinlock::test_and_set(bool* pointer_to_lock_flag, bool new_value) {
+    /* In x86, when an instruction is prefixed with `lock`, it is guaranteed to be run atomically (thread-safe).
+     * The `xchg` instruction sets a memory address or register to a new value while returning the old value in RAX.
+     * It can be determined that the lock *was* free (unlocked) and *is* acquired (locked)
+     *   by currently executing code by testing that the old value that is returned is equal to false.
+     */
+    
+     /* Inline Assembly:
+     * |- Input(s)
+     * |  |- "m" -> Memory Address of lock flag
+     * |  `- "r" -> Register of new lock flag value
+     * `- Output(s)
+     *    `- "=a" -> Write-only; output in RAX
+     */
     bool old_value;
     asm volatile ("lock xchg %0, %1" : "=a" (old_value) : "m" (pointer_to_lock_flag), "r" (new_value));
     return old_value;
