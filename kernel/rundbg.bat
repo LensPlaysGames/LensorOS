989840c3a698fa83191147e2919a4f5d96410b8c
LensPlaysGames <lensplaysgames@gmail.com>
2022-02-21 22:52:53 -0800

    Update rundbg.bat
    Setup dbg run script to use new copied `OVMF_VARS.fd` that isn't tracked in git.

--- kernel/rundbg.bat
+++ kernel/rundbg.bat
@@ -1,8 +1,6 @@
-set OSNAME=LensorOS
-set BUILDDIR=%0/../bin
-set OVMFDIR=%0/../../OVMFbin
-
-set BUILDDIR=%BUILDDIR:"=%
-set OVMFDIR=%OVMFDIR:"=%
-qemu-system-x86_64 -s -S -m 100M -cpu qemu64 -vga cirrus -rtc base=localtime,clock=host,driftfix=none -machine q35 -serial stdio -net none -d cpu_reset -drive format=raw,file=%BUILDDIR%/%OSNAME%.img -drive if=pflash,format=raw,unit=0,file=%OVMFDIR%/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=%OVMFDIR%/OVMF_VARS-pure-efi.fd
-pause
+set BuildDirectory=%0/../bin
+set OVMFbin=%0/../../OVMFbin
+set BuildDirectory=%BuildDirectory:"=%
+set OVMFbin=%OVMFbin:"=%
+IF NOT EXIST %OVMFbin%/OVMF_VARS_LensorOS.fd cp %OVMFbin%/OVMF_VARS-pure-efi.fd %OVMFbin%/OVMF_VARS_LensorOS.fd
+qemu-system-x86_64 -s -S -cpu qemu64 -m 100M -rtc base=localtime,clock=host,driftfix=none -machine q35 -serial stdio -vga cirrus -d cpu_reset -drive format=raw,file=%BuildDirectory%/LensorOS.img -drive if=pflash,format=raw,unit=0,file=%OVMFbin%/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=%OVMFbin%/OVMF_VARS_LensorOS.fd -net none
