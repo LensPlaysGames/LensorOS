83a10195b50cba519d13b0e1649745712bb96a44
LensPlaysGames <lensplaysgames@gmail.com>
2022-03-01 18:11:09 -0800

    Break super long script lines into multiple lines
    This improves readability by like %1000!!!

--- kernel/run.sh
+++ kernel/run.sh
@@ -5,4 +5,16 @@ OVMFDirectory="$ScriptDirectory/../OVMFbin"
 if ! [ -f $OVMFDirectory/OVMF_VARS_LensorOS.fd ] ; then
     cp $OVMFDirectory/OVMF_VARS-pure-efi.fd $OVMFDirectory/OVMF_VARS_LensorOS.fd
 fi
-qemu-system-x86_64 -cpu qemu64 -m 100M -rtc base=localtime,clock=host,driftfix=none -machine q35 -serial stdio -vga cirrus -soundhw pcspk -d cpu_reset -drive format=raw,file=$BuildDirectory/LensorOS.img -drive if=pflash,format=raw,unit=0,file=$OVMFDirectory/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=$OVMFDirectory/OVMF_VARS_LensorOS.fd -net none
+qemu-system-x86_64 \
+    -d cpu_reset \
+    -machine q35 \
+    -cpu qemu64 \
+    -m 100M \
+    -serial stdio \
+    -vga cirrus \
+    -soundhw pcspk \
+    -net none \
+    -rtc base=localtime,clock=host,driftfix=none \
+    -drive format=raw,file=$BuildDirectory/LensorOS.img \
+    -drive if=pflash,format=raw,unit=0,file=$OVMFDirectory/OVMF_CODE-pure-efi.fd,readonly=on \
+    -drive if=pflash,format=raw,unit=1,file=$OVMFDirectory/OVMF_VARS_LensorOS.fd
