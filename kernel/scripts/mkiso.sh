9522754163f7723b1e5b6b0881f52a10c8b338de
LensPlaysGames <lensplaysgames@gmail.com>
2022-03-17 14:26:24 -0700

    Update mkiso.sh
    This script now utilizes the same `run` function/macro/thing that the other scripts do, which allows the user to see exactly what commands are being run in a nicely formatted way on the command line.

--- kernel/scripts/mkiso.sh
+++ kernel/scripts/mkiso.sh
@@ -1,7 +1,14 @@
 ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BuildDirectory="$ScriptDirectory/../bin"
 $ScriptDirectory/mkimg.sh
-mkdir -p $BuildDirectory/iso
-cp $BuildDirectory/LensorOS.img $BuildDirectory/iso
-xorriso -as mkisofs -R -f -e LensorOS.img -no-emul-boot -o $BuildDirectory/LensorOS.iso $BuildDirectory/iso
-rm -r $BuildDirectory/iso
+
+run(){
+    set -x
+    "$@"
+    { set +x; } 2>/dev/null
+}
+
+run mkdir -p $BuildDirectory/iso
+run cp $BuildDirectory/LensorOS.img $BuildDirectory/iso
+run xorriso -as mkisofs -R -f -e LensorOS.img -no-emul-boot -o $BuildDirectory/LensorOS.iso $BuildDirectory/iso
+run rm -r $BuildDirectory/iso
