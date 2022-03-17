950aee1ba0d0754d9c1081c71b196a04fa4fbed0
LensPlaysGames <lensplaysgames@gmail.com>
2022-03-17 14:27:56 -0700

    Create Linux `mkgpt` install helper script
    If on Linux with the appropriate dependencies installed, everything should go smoothly, and you will end up with a working executable that you can add to your path in the `mkgpt` subdirectory.

---
+++ kernel/scripts/install_mkgpt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+run(){
+    set -x
+    "$@"
+    { set +x; } 2>/dev/null
+}
+
+run sudo apt install autoconf automake
+run git clone https://github.com/jncronin/mkgpt.git
+run cd mkgpt
+run automake --add-missing
+run autoreconf
+run ./configure
+run make
+run sudo make install
