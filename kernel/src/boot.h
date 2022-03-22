41836a063fe3b2fb591cfce63161a0dbee6bbff1
LensPlaysGames <lensplaysgames@gmail.com>
2022-03-21 17:53:34 -0700

    Create boot.h

---
+++ kernel/src/boot.h
@@ -0,0 +1,18 @@
+#ifndef LENSOR_OS_BOOT_H
+#define LENSOR_OS_BOOT_H
+
+#include "acpi.h"
+#include "basic_renderer.h"
+#include "efi_memory.h"
+#include "integers.h"
+
+struct BootInfo {
+    Framebuffer* framebuffer;
+    PSF1_FONT* font;
+    EFI_MEMORY_DESCRIPTOR* map;
+    u64 mapSize;
+    u64 mapDescSize;
+    ACPI::RSDP2* rsdp;
+};
+
+#endif /* LENSOR_OS_BOOT_H */
