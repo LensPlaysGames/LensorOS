6f603938fecfaaeaf135808e1379e8df0e1b21d4
LensPlaysGames <lensplaysgames@gmail.com>
2022-03-22 18:31:08 -0700

    Create runhdadbg.bat
    It's a Windows batch script that runs QEMU using the `-S` and `-s` flags, both needed for debugging with GDB.

---
+++ kernel/scripts/runhdadbg.bat
@@ -0,0 +1,18 @@
+SET BuildDirectory=%0/../../bin
+SET OVMFbin=%0/../../../OVMFbin
+SET BuildDirectory=%BuildDirectory:"=%
+SET OVMFbin=%OVMFbin:"=%
+IF NOT EXIST %OVMFbin%/OVMF_VARS_LensorOS.fd cp %OVMFbin%/OVMF_VARS-pure-efi.fd %OVMFbin%/OVMF_VARS_LensorOS.fd
+qemu-system-x86_64.exe ^
+ -S -s ^
+ -cpu qemu64 ^
+ -machine q35 ^
+ -m 100M ^
+ -rtc base=localtime,clock=host,driftfix=none ^
+ -serial stdio ^
+ -vga cirrus ^
+ -soundhw pcspk ^
+ -d cpu_reset ^
+ -hda %BuildDirectory%\LensorOS.bin ^
+ -drive if=pflash,format=raw,unit=0,file=%OVMFbin%\OVMF_CODE-pure-efi.fd,readonly=on ^
+ -drive if=pflash,format=raw,unit=1,file=%OVMFbin%\OVMF_VARS_LensorOS.fd -net none
