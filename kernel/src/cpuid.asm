831437483a2c8895c5d143587f2b94f7c829d9de
LensPlaysGames <lensplaysgames@gmail.com>
2022-03-22 19:06:34 -0700

    Update cpuid.asm
    Formatting, mainly

--- kernel/src/cpuid.asm
+++ kernel/src/cpuid.asm
@@ -1,15 +1,13 @@
-    [BITS 64]
-    
-    GLOBAL cpuid_support
-
+[BITS 64]    
+SECTION .text
+GLOBAL cpuid_support
 cpuid_support:
-    
     pushfq                      ; Save RFLAGS
-    
+
     pushfq                      ; Store RFLAGS on stack
     xor QWORD [rsp], 0x00200000 ; Invert stored EFLAGS 'ID' bit
     popfq                       ; Try to load stored RFLAGS with modified 'ID' bit
-    
+
     pushfq                      ; Store RFLAGS on stack to check if 'ID' has been changed
     pop rax                     ; Store RFLAGS from stack into RAX
     
