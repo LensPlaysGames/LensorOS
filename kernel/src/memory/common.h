8bf32778711e56f1f8908ab97ab78c13ff1e20c0
Lens <LensPlaysGames@gmail.com>
2022-03-24 03:31:42 -0700

    Byte size conversion directives
    It gets annoying having to switch to python in a terminal or something to figure out how many bytes 8GB is all the time, so why not just write the code to do it for me?

--- kernel/src/memory/common.h
+++ kernel/src/memory/common.h
@@ -1,6 +1,12 @@
 #ifndef LENSOR_OS_MEMORY_COMMON_H
 #define LENSOR_OS_MEMORY_COMMON_H
 
-#define PAGE_SIZE 4096
+#include "../integers.h"
+
+#define KB(x) ((u64)(x) << 10)
+#define MB(x) ((u64)(x) << 20)
+#define GB(x) ((u64)(x) << 30)
+
+constexpr u64 PAGE_SIZE = 4096;
 
 #endif /* LENSOR_OS_MEMORY_COMMON_H */
