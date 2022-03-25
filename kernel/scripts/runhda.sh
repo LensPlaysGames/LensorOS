18164e1d3de8fda88ef3a8e814c9f727982b2730
Lens <LensPlaysGames@gmail.com>
2022-03-25 09:33:37 -0700

    Fix Broken Run Script
    It's amazing that this has went un-noticed this long! I even had stuff from RADII in there somehow :^<

--- kernel/scripts/runhda.sh
+++ kernel/scripts/runhda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TopLevelDirectory="$ScriptDirectory/.."
-OVMFDirectory="$TopLevelDirectory/OVMFbin"
+KernelDirectory="$ScriptDirectory/.."
+OVMFDirectory="$KernelDirectory/../OVMFbin"
 if ! [ -f $OVMFDirectory/OVMF_VARS_LensorOS.fd ] ; then
     cp $OVMFDirectory/OVMF_VARS-pure-efi.fd $OVMFDirectory/OVMF_VARS_LensorOS.fd
 fi
@@ -14,6 +14,6 @@ qemu-system-x86_64 \
     -vga cirrus \
     -soundhw pcspk \
     -rtc base=localtime,clock=host,driftfix=none \
-    -hda $TopLevelDirectory/bin/LensorOS.bin \
+    -hda $KernelDirectory/bin/LensorOS.bin \
     -drive if=pflash,format=raw,unit=0,file=$OVMFDirectory/OVMF_CODE-pure-efi.fd,readonly=on \
-    -drive if=pflash,format=raw,unit=1,file=$OVMFDirectory/OVMF_VARS_RADII.fd -net none
+    -drive if=pflash,format=raw,unit=1,file=$OVMFDirectory/OVMF_VARS_LensorOS.fd -net none
