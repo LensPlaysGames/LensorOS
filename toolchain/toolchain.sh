32f1611fc0ca26ac1a13d368a204c46653712641
Lens <LensPlaysGames@gmail.com>
2022-03-31 13:23:12 -0700

    Toolchain: Update `toolchain.sh`

--- toolchain/toolchain.sh
+++ toolchain/toolchain.sh
@@ -3,7 +3,12 @@
 # |-- Curl
 # |-- GNU Tar (xz-utils)
 # `-- GNU Diff/Patch
+# Set variables.
+TARGET="x86_64-lensoros-elf"
+PREFIX="$ScriptDirectory/cross"
+SYSROOT="$ScriptDirectory/../root"
 ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Ensure known working directory.
 cd $ScriptDirectory
 # Download source archives.
 echo -e "\n\n -> Downloading GNU Binutils Source Archive\n\n"
@@ -19,23 +24,33 @@ tar -xf binutils-2.38.tar.xz -v -C .
 echo -e "\n\n -> Extracting GNU Compiler Collection\n\n"
 mkdir -p "gcc-11.2.0"
 tar -xf gcc-11.2.0.tar.xz -v -C .
+# Patch Binutils.
+echo -e "\n\n -> Patching GNU Binutils\n\n"
+patch -s -u -p0 < $ScriptDirectory/binutils-2.38-lensor.patch
 # Patch GCC.
 echo -e "\n\n -> Patching GNU Compiler Collection\n\n"
-patch -s -u -p0 < $ScriptDirectory/gcc-11.2.0.patch
-# Create build directories.
+patch -s -u -p0 < $ScriptDirectory/gcc-11.2.0-lensor.patch
+# Bootstrap sysroot.
+if [[ ! -d "$SYSROOT" ]] ; then
+    mkdir -p "$SYSROOT"
+	# Copy base filesystem with pre-built libc
+	#     binaries into newly-created sysroot.
+	# This solves the issue of having to build a
+	#     bootstrap version of the compiler first.
+	cp -r ../base "$SYSROOT"
+	# Use libc install script to install system headers.
+	source ../user/libc/install.sh "$SYSROOT"
+fi
+# Create output build directory.
 mkdir -p cross
-mkdir -p binutils-build
-mkdir -p gcc-build
-# Set variables.
-PREFIX="$ScriptDirectory/cross"
-TARGET="x86_64-lensoros-elf"
 # Configure binutils.
 echo -e "\n\n -> Configuring GNU Binutils\n\n"
+mkdir -p binutils-build
 cd binutils-build
 $ScriptDirectory/binutils-2.38/configure \
     --target=$TARGET \
     --prefix="$PREFIX" \
-    --with-sysroot \
+    --with-sysroot="$SYSROOT" \
     --disable-nls \
     --disable-werror
 # Build binutils.
@@ -44,12 +59,17 @@ make -j
 make install -j
 # Configure GCC.
 echo -e "\n\n -> Configuring GNU Compiler Collection\n\n"
+mkdir -p gcc-build
 cd $ScriptDirectory/gcc-build
-$ScriptDirectory/gcc-11.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+$ScriptDirectory/gcc-11.2.0/configure \
+	--target=$TARGET \
+	--prefix="$PREFIX" \
+	--disable-nls \
+	--enable-languages=c,c++ \
+	--with-sysroot="$SYSROOT"
 # Build GCC.
 echo -e "\n\n -> Building & Installing GNU Compiler Collection\n\n"
 make all-gcc -j
 make all-target-libgcc -j
 make install-gcc -j
 make install-target-libgcc -j
-
