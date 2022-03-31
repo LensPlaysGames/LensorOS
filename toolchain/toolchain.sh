27d9ab7a89d4e13bae2f134fcd6cc4f467c17722
Lens <LensPlaysGames@gmail.com>
2022-03-31 10:04:13 -0700

    Toolchain: Output status as script goes on
    This makes it more obvious what the script is currently doing (and why it's taking so long :^)

--- toolchain/toolchain.sh
+++ toolchain/toolchain.sh
@@ -6,16 +6,21 @@
 ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $ScriptDirectory
 # Download source archives.
+echo -e "\n\n -> Downloading GNU Binutils Source Archive\n\n"
 curl https://mirrors.kernel.org/gnu/binutils/binutils-2.38.tar.xz \
      --output binutils-2.38.tar.xz
+echo -e "\n\n -> Downloading GNU Compiler Collection Source Archive\n\n"
 curl https://mirrors.kernel.org/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.xz \
      --output gcc-11.2.0.tar.xz
 # Extract archives into source directories.
+echo -e "\n\n -> Extracting GNU Binutils\n\n"
 mkdir -p "binutils-2.38"
 tar -xf binutils-2.38.tar.xz -v -C .
+echo -e "\n\n -> Extracting GNU Compiler Collection\n\n"
 mkdir -p "gcc-11.2.0"
 tar -xf gcc-11.2.0.tar.xz -v -C .
 # Patch GCC.
+echo -e "\n\n -> Patching GNU Compiler Collection\n\n"
 patch -s -u -p0 < $ScriptDirectory/gcc-11.2.0.patch
 # Create build directories.
 mkdir -p cross
@@ -25,6 +30,7 @@ mkdir -p gcc-build
 PREFIX="$ScriptDirectory/cross"
 TARGET="x86_64-lensoros-elf"
 # Configure binutils.
+echo -e "\n\n -> Configuring GNU Binutils\n\n"
 cd binutils-build
 $ScriptDirectory/binutils-2.38/configure \
     --target=$TARGET \
@@ -33,12 +39,15 @@ $ScriptDirectory/binutils-2.38/configure \
     --disable-nls \
     --disable-werror
 # Build binutils.
+echo -e "\n\n -> Building & Installing GNU Binutils\n\n"
 make -j
 make install -j
 # Configure GCC.
+echo -e "\n\n -> Configuring GNU Compiler Collection\n\n"
 cd $ScriptDirectory/gcc-build
 $ScriptDirectory/gcc-11.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 # Build GCC.
+echo -e "\n\n -> Building & Installing GNU Compiler Collection\n\n"
 make all-gcc -j
 make all-target-libgcc -j
 make install-gcc -j
