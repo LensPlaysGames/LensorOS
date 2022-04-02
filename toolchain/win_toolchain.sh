c72c2ceaf6199a379fb39cec2b7b769c13eebeab
Lens <LensPlaysGames@gmail.com>
2022-04-02 12:33:10 -0700

    Toolchain: Windows Canadian Cross
    Yes, it's correct; the LensorOS toolchain is officially running on Windows! More specifically, the `x86_64-w64-mingw32` host triplet. The toolchain binaries themselves must be built on Linux, but run on Windows, and generate executables for LensorOS, meaning this is a true Canadian Cross compiler (I always loved triple `C`s :^).
    
    I think it's about time to provide pre-built toolchain binaries for both Windows and Linux; that would greatly simplify any onboarding for contributors, as well as make it easier to use the OS (without providing pre-built disk images; we're not quite that stable yet), but there are still some major issues with the system root configuration. The sysroot issues are temporarily fixed for freestanding programs (i.e. kernel only) using the `-I` command line flag; this will not work for userland programs due to system libraries not being present in the incorrect sysroot path.
    
    As for solutions, it's possible the `--with-build-sysroot` flag could help, but documentation isn't clear on what it does; I guess it's time for me to start fiddling with the toolchain script that takes 45+ minutes to run each time... FUN!

---
+++ toolchain/win_toolchain.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+#     This script is meant to be ran from linux-gnu,
+#     and creates the LensorOS toolchain for w64-mingw32.
+
+#     Dependencies:
+#     |-- Curl
+#     |-- GNU Tar (xz-utils)
+#     `-- GNU Diff/Patch
+
+# Set variables.
+ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TARGET="x86_64-lensor"
+PREFIX="$ScriptDirectory/wincross"
+SYSROOT="$ScriptDirectory/../root"
+# Ensure known working directory.
+cd $ScriptDirectory
+# Download and extract source archives if they haven't been already.
+if [ ! -d "binutils-2.38" ]; then
+    echo -e "\n\n -> Downloading GNU Binutils Source Archive\n\n"
+    curl https://mirrors.kernel.org/gnu/binutils/binutils-2.38.tar.xz \
+         --output binutils-2.38.tar.xz
+fi
+if [ ! -d "gcc-11.2.0" ]; then
+    echo -e "\n\n -> Downloading GNU Compiler Collection Source Archive\n\n"
+    curl https://mirrors.kernel.org/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.xz \
+         --output gcc-11.2.0.tar.xz
+fi
+# Extract archives into source directories.
+if [ ! -d "binutils-2.38" ]; then
+    echo -e "\n\n -> Extracting GNU Binutils\n\n"
+    mkdir -p "binutils-2.38"
+    tar -xf binutils-2.38.tar.xz -C .
+fi
+if [ ! -d "gcc-11.2.0" ]; then
+    echo -e "\n\n -> Extracting GNU Compiler Collection\n\n"
+    mkdir -p "gcc-11.2.0"
+    tar -xf gcc-11.2.0.tar.xz -C .
+    echo -e "\n\n -> Downloading GNU Compiler Collection Prerequisites\n\n"
+    cd gcc-11.2.0
+    ./contrib/download_prerequisites
+    cd $ScriptDirectory
+fi
+# Patch source code.
+if [ ! -d "binutils-2.38" ]; then
+    echo -e "\n\n -> Patching GNU Binutils\n\n"
+    patch -s -u -p0 < $ScriptDirectory/binutils-2.38-lensor.patch
+fi
+if [ ! -d "gcc-11.2.0" ]; then
+    echo -e "\n\n -> Patching GNU Compiler Collection\n\n"
+    patch -s -u -p0 < $ScriptDirectory/gcc-11.2.0-lensor.patch
+fi
+# Bootstrap sysroot.
+if [ ! -d "$SYSROOT" ] ; then
+    echo -e "\n\n -> Bootstrapping system root at $SYSROOT\n\n"
+    mkdir -p "$SYSROOT"
+    # Copy base filesystem with pre-built libc
+    #     binaries into newly-created sysroot.
+    # This solves the issue of having to build a
+    #     bootstrap version of the compiler first.
+    cp -r ../base/* "$SYSROOT/"
+    # Copy header files from libc to sysroot.
+    cd ../user/libc/
+    find ./ -name '*.h' -exec cp --parents '{}' -t $SYSROOT/inc ';'
+    cd $ScriptDirectory
+fi
+# Create output build directory.
+mkdir -p wincross
+# Configure binutils.
+echo -e "\n\n -> Configuring GNU Binutils\n\n"
+mkdir -p binutils-winbuild
+cd binutils-winbuild
+$ScriptDirectory/binutils-2.38/configure \
+    --build x86_64-linux-gnu             \
+    --host x86_64-w64-mingw32            \
+    --target=$TARGET                     \
+    --prefix="$PREFIX"                   \
+    --with-sysroot="$SYSROOT"            \
+    --disable-nls                        \
+    --disable-werror
+# Build binutils.
+echo -e "\n\n -> Building & Installing GNU Binutils\n\n"
+make -j
+make install -j
+# Configure GCC.
+echo -e "\n\n -> Configuring GNU Compiler Collection\n\n"
+cd $ScriptDirectory
+mkdir -p gcc-winbuild
+cd gcc-winbuild
+$ScriptDirectory/gcc-11.2.0/configure \
+    --build x86_64-linux-gnu          \
+    --host x86_64-w64-mingw32         \
+    --target=$TARGET                  \
+    --prefix="$PREFIX"                \
+    --disable-nls                     \
+    --enable-languages=c,c++          \
+    --with-sysroot="$SYSROOT"
+# Build GCC.
+echo -e "\n\n -> Building & Installing GNU Compiler Collection\n\n"
+make all-gcc -j
+make all-target-libgcc -j
+make install-gcc -j
+make install-target-libgcc -j
