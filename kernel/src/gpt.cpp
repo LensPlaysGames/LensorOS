8f4066914a5535abe431d29bf92a9f5c9f4f1060
Lens <LensPlaysGames@gmail.com>
2022-04-19 19:31:48 -0700

    GPT: Use `dbgmsg()` over `UART::out`

--- kernel/src/gpt.cpp
+++ kernel/src/gpt.cpp
@@ -1,8 +1,8 @@
 #include <gpt.h>
 
 #include <ahci.h>
+#include <debug.h>
 #include <smart_pointer.h>
-#include <uart.h>
 
 // Uncomment the following directive for extra debug information output.
 //#define DEBUG_GPT
@@ -10,33 +10,32 @@
 namespace GPT {
     constexpr const char* HEADER_SIGNATURE = "EFI PART";
     bool is_gpt_present(StorageDeviceDriver* driver) {
-#ifdef DEBUG_GPT
-        UART::out("[GPT]: Checking for valid GPT\r\n");
-#endif /* DEBUG_GPT */
         if (driver == nullptr) {
-            UART::out("  Driver is nullptr\r\n");
+            dbgmsg("[GPT]: GPT can not be present on driver that is nullptr!\r\n");
             return false;
         }
-
+#ifdef DEBUG_GPT
+        dbgmsg("[GPT]: Checking for valid GPT\r\n");
+#endif /* DEBUG_GPT */
         SmartPtr<Header> hdr = SmartPtr<Header>(new Header);
         driver->read(512, sizeof(Header), (u8*)hdr.get());
         // Validate GPT Header
         if (hdr->Revision == 0) {
 #ifdef DEBUG_GPT
-            UART::out("  Revision is not zero\r\n");
+            dbgmsg("  ERROR: Revision is not zero\r\n");
 #endif /* DEBUG_GPT */
             return false;
         }
         for (u8 i = 0; i < 8; ++i) {
             if (hdr->Signature[i] != HEADER_SIGNATURE[i]) {
 #ifdef DEBUG_GPT
-                UART::out("  Signature doesn't match\r\n");
+                dbgmsg("  ERROR: Signature doesn't match\r\n");
 #endif /* DEBUG_GPT */
                 return false;
             }
         }
 #ifdef DEBUG_GPT
-        UART::out("  Valid GPT\r\n");
+        dbgmsg("  Valid GPT\r\n");
 #endif /* DEBUG_GPT */
         return true;
     }
