03e8633ef3f8946b88094347008aaaa2040135bb
Lens <LensPlaysGames@gmail.com>
2022-04-26 16:59:37 -0700

    Untabify `mkimg.sh`
    Tabs are evil!

--- scripts/mkimg.sh
+++ scripts/mkimg.sh
@@ -12,20 +12,20 @@ run(){
 
 # Ensure existence of files used.
 if [ ! -f "$BootloaderEFI" ]; then
-	echo -e "\n\n -> ERROR: Could not locate bootloader executable at $BootloaderEFI\n\n"
-	exit 1
+    echo -e "\n\n -> ERROR: Could not locate bootloader executable at $BootloaderEFI\n\n"
+    exit 1
 fi
 if [ ! -f "$BuildDirectory/kernel.elf" ]; then
-	echo -e "\n\n -> ERROR: Could not locate kernel executable at $BuildDirectory/kernel.elf\n\n"
-	exit 1
+    echo -e "\n\n -> ERROR: Could not locate kernel executable at $BuildDirectory/kernel.elf\n\n"
+    exit 1
 fi
 if [ ! -f "$BuildDirectory/dfltfont.psf" ]; then
-	echo -e "\n\n -> ERROR: Could not locate font at $BuildDirectory/dfltfont.psf\n\n"
-	exit 1
+    echo -e "\n\n -> ERROR: Could not locate font at $BuildDirectory/dfltfont.psf\n\n"
+    exit 1
 fi
 if [ ! -f "$ScriptDirectory/startup.nsh" ]; then
-	echo -e "\n\n -> ERROR: Could not locate UEFI startup script at $ScriptDirectory/startup.nsh\n\n"
-	exit 1
+    echo -e "\n\n -> ERROR: Could not locate UEFI startup script at $ScriptDirectory/startup.nsh\n\n"
+    exit 1
 fi
 
 run mkdir -p $BuildDirectory
