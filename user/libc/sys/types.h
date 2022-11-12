198df264b2202006c84df0120dee8f3be2420b87
Sirraide <aeternalmail@gmail.com>
2022-11-12 22:39:51 +0100

    libc: Update sys/types.h

--- user/libc/sys/types.h
+++ user/libc/sys/types.h
@@ -21,39 +21,39 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
+#include <bits/decls.h>
 #include <stdint.h>
 
-#if defined (__cplusplus)
-extern "C" {
-#endif
+__BEGIN_DECLS__
 
 #define unsigned signed
-  typedef __SIZE_TYPE__ ssize_t;
+typedef __SIZE_TYPE__ ssize_t;
 #undef unsigned
 
-  typedef int64_t blkcnt_t;
-  typedef int64_t blksize_t;
-  typedef uint64_t clock_t;
-  typedef uint64_t clockid_t;
-  typedef uint64_t dev_t;
-  typedef uint64_t fsblkcnt_t;
-  typedef uint64_t fsfilcnt_t;
-  typedef uint64_t fpos_t;
-  typedef uint64_t gid_t;
-  typedef uint64_t id_t;
-  typedef uint64_t ino_t;
-  typedef uint64_t mode_t;
-  typedef uint64_t nlink_t;
-  typedef int64_t off_t;
-  typedef int64_t pid_t;
-  typedef uint64_t size_t;
-  typedef int64_t ssize_t;
-  typedef uint64_t time_t;
-  typedef uint64_t timer_t;
-  typedef uint64_t uid_t;
-
-#if defined (__cplusplus)
-} /* extern "C" */
+#ifndef __kernel__
+typedef int64_t pid_t;
 #endif
+
+typedef int64_t blkcnt_t;
+typedef int64_t blksize_t;
+typedef uint64_t clock_t;
+typedef uint64_t clockid_t;
+typedef uint64_t dev_t;
+typedef uint64_t fsblkcnt_t;
+typedef uint64_t fsfilcnt_t;
+typedef uint64_t fpos_t;
+typedef uint64_t gid_t;
+typedef uint64_t id_t;
+typedef uint64_t ino_t;
+typedef uint64_t mode_t;
+typedef uint64_t nlink_t;
+typedef int64_t off_t;
+typedef uint64_t size_t;
+typedef int64_t ssize_t;
+typedef uint64_t time_t;
+typedef uint64_t timer_t;
+typedef uint64_t uid_t;
+
+__END_DECLS__
 
 #endif /* _TYPES_H */
