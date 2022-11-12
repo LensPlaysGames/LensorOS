59e0ea48de052acd4af711527584b3bb2c8d86e9
Sirraide <aeternalmail@gmail.com>
2022-11-12 22:40:43 +0100

    libc: Fix install_headers()

--- user/libc/install-headers.cmake
+++ user/libc/install-headers.cmake
@@ -1,11 +1,11 @@
 function(install_headers)
-    set(dest_dir ${CMAKE_CURRENT_SOURCE_DIR}/../../root/inc)
+    set(dest_dir $ENV{SOURCE_DIR}/../../root/inc)
 
-    file(GLOB libc_headers "${CMAKE_CURRENT_SOURCE_DIR}/include/*.h")
-    file(GLOB libc_bits_headers "${CMAKE_CURRENT_SOURCE_DIR}/include/bits/*.h")
-    file(GLOB libc_sys_headers "${CMAKE_CURRENT_SOURCE_DIR}/include/sys/*.h")
-    file(GLOB libcxx_headers "${CMAKE_CURRENT_SOURCE_DIR}/../../std/*")
-    file(GLOB libcxx_bits_headers "${CMAKE_CURRENT_SOURCE_DIR}/../../std/bits/*.h")
+    file(GLOB libc_headers "$ENV{SOURCE_DIR}/*.h")
+    file(GLOB libc_bits_headers "$ENV{SOURCE_DIR}/bits/*.h")
+    file(GLOB libc_sys_headers "$ENV{SOURCE_DIR}/sys/*.h")
+    file(GLOB libcxx_headers "$ENV{SOURCE_DIR}/../../std/*")
+    file(GLOB libcxx_bits_headers "$ENV{SOURCE_DIR}/../../std/bits/*.h")
 
     file(COPY ${libc_headers} DESTINATION "${dest_dir}")
     file(COPY ${libc_bits_headers} DESTINATION "${dest_dir}/bits")
