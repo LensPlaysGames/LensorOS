4c156d579ff77d0018cbc5b3fcea155f9d750a6f
Lens <LensPlaysGames@gmail.com>
2023-01-28 12:31:16 -0800

    [Userspace] Update `stdout`
    It actually collects commands now! So that's cool.

--- user/stdout/main.c
+++ user/stdout/main.c
@@ -19,47 +19,97 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/syscalls.h>
 
+#ifndef MAX_COMMAND_LENGTH
+# define MAX_COMMAND_LENGTH 4096
+#endif
+
+static char command[MAX_COMMAND_LENGTH];
+
+#define ESCAPE     0x01
+#define BACKSPACE  0x0e
+#define TAB        0x0f
+#define ENTER      0x1c
+#define LCONTROL   0x1d
+#define LSHIFT     0x2a
+#define RSHIFT     0x36
+#define LALT       0x38
+#define SPACE      0x39
+#define CAPSLOCK   0x3a
+#define NUMLOCK    0x45
+#define SCROLLLOCK 0x46
+
+/// Preceded by 'e0' byte.
+#define ARROW_UP    0x48
+#define ARROW_DOWN  0x50
+#define ARROW_LEFT  0x4b
+#define ARROW_RIGHT 0x4d
+
 int main(int argc, const char **argv) {
-  //const char *message = "Hello, friends :^)";
-  //puts(message);
+  puts("Arguments:");
   for (int i = 0; i < argc; i++) puts(argv[i]);
+  fflush(NULL);
 
   for (;;) {
-    // Get character from standard input.
-    int c = getchar();
-    // Skip error return value.
-    if (c == EOF) {
-      // TODO: Wait/waste some time so we don't choke the system just
-      // spinning.
-      continue;
+    memset(command, 0, MAX_COMMAND_LENGTH);
 
-    } else if (c == 'a') {
+    // Get line from standard input.
+    int c;
+    int offset = 0;
+    while ((c = getchar()) != '\n') {
+      if (c == EOF) {
+        // TODO: Wait/waste some time so we don't choke the system just
+        // spinning.
+        continue;
+      }
+      if (c == '\b' && offset > 0) {
+        command[--offset] = '\0';
+        // Echo command to standard out.
+        puts(command);
+        fflush(NULL);
+        continue;
+      }
+      if (offset >= MAX_COMMAND_LENGTH) {
+        puts("Reached max command length, discarding command.");
+        offset = 0;
+        command[offset] = '\0';
+        continue;
+      }
+      command[offset++] = c;
+      // Echo command to standard out.
+      puts(command);
+      fflush(NULL);
+    }
+    command[offset] = '\0';
+
+    if (strcmp(command, "blazeit") == 0) {
       // If there are pending writes, they will be executed on both the
       // parent and the child; by flushing any buffers we have, it ensures
       // the child won't write duplicate data on accident.
       pid_t cpid = syscall(SYS_fork);
       if (cpid) {
-        puts("Parent");
-        fflush(NULL);
+        //puts("Parent");
+        //fflush(NULL);
         syscall(SYS_waitpid, cpid);
-        puts("Parent waited");
-        fflush(NULL);
+        //puts("Parent waited");
+        //fflush(NULL);
       } else {
-        puts("Child");
-        fflush(NULL);
+        //puts("Child");
+        //fflush(NULL);
         syscall(SYS_exec, "/fs0/blazeit");
-        fflush(NULL);
       }
-    } else if (c == 'q') {
-      puts("Quitting.");
+      continue;
+    } else if (strcmp(command, "quit") == 0) {
+      puts("Shell quitting, baiBAI!\n");
+      fflush(NULL);
       break;
     }
-    // Echo character to standard out.
-    putc((char)c, stdout);
+    puts("Unrecognized command, sorry!\n");
     fflush(NULL);
+    continue;
   }
 
   return 0;
