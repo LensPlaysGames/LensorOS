79eceb24345e0c0c46c1f50d374a7bf6c3df1b27
Lens_r <lensplaysgames@gmail.com>
2026-09-03 19:55:04 -0700

    Serving the Client

--- user/clienttest/main.c
+++ user/clienttest/main.c
@@ -1,62 +1,68 @@
-#include <sys/syscalls.h>
-
 #include <stdio.h>
 #include <string.h>
+#include <sys/syscalls.h>
 #include <unistd.h>
 
-int main(int argc, char **argv) {
-  printf("[CLIENT]: starting... opening socket\n");
-  fflush(stdout);
-
-  int sockFD = sys_socket(0, 0, 0);
-
-  printf("[CLIENT]: socket opened\n");
-  fflush(stdout);
-
-  sockaddr addr;
-  addr.type = LENSOR16;
-  const char socket_path[] = "!Test";
-  memset(addr.data, 0, SOCK_ADDR_MAX_SIZE);
-  memcpy(addr.data, &socket_path, sizeof(socket_path) - 1);
-  int rc = sys_connect(sockFD, &addr, sizeof(sockaddr));
-  if (rc) {
-    close(sockFD);
-    printf("[CLIENT]: Couldn't connect to address: %s\n", socket_path);
+int main(int argc, char** argv) {
+    printf("[CLIENT]: starting... opening socket\n");
     fflush(stdout);
-    return rc;
-  }
-  printf("[CLIENT]: Connected to address: \"%s\"\n", socket_path);
-  fflush(stdout);
 
-  printf("[CLIENT]: Reading from socket...\n");
-  fflush(stdout);
+    int sockFD = sys_socket(0, 0, 0);
 
-  unsigned char data[512];
-  int bytes_read = 0;
-  bytes_read += read(sockFD, data, 16);
+    printf("[CLIENT]: socket opened\n");
+    fflush(stdout);
 
-  printf("[CLIENT]: Read %d bytes from socket\n", bytes_read);
-  fflush(stdout);
+    sockaddr addr;
+    addr.type = LENSOR16;
+    const char socket_path[] = "!Test";
+    memset(addr.data, 0, SOCK_ADDR_MAX_SIZE);
+    memcpy(addr.data, &socket_path, sizeof(socket_path) - 1);
+    int rc = sys_connect(sockFD, &addr, sizeof(sockaddr));
+    if (rc) {
+        close(sockFD);
+        printf("[CLIENT]: Couldn't connect to address: %s\n", socket_path);
+        fflush(stdout);
+        return rc;
+    }
+    printf("[CLIENT]: Connected to address: \"%s\"\n", socket_path);
+    fflush(stdout);
 
-  uint64_t* data_it = (uint64_t*)data;
-  uint64_t leading = *data_it++;
-  uint64_t trailing = *data_it;
+    printf("[CLIENT]: Reading from socket...\n");
+    fflush(stdout);
 
-  printf("[CLIENT]:  got %u and %u\n", leading, trailing);
-  fflush(stdout);
+    unsigned char data[512];
+    int bytes_read = 0;
+    bytes_read += read(sockFD, data, 24);
 
-  write(sockFD, data, 16);
+    printf("[CLIENT]: Read %d bytes from socket\n", bytes_read);
+    fflush(stdout);
 
-  printf("[CLIENT]:  client written\n");
-  fflush(stdout);
+    uint64_t* data_it = (uint64_t*)data;
+    uint64_t leading = *data_it++;
+    uint64_t trailing = *data_it++;
+    uint64_t shared_memory_id = *data_it++;
 
-  printf("[CLIENT]:  closing client socket\n");
-  fflush(stdout);
+    printf("[CLIENT]:  got %u and %u\n", leading, trailing);
+    printf("[CLIENT]:  shared memory id %u\n", shared_memory_id);
+    fflush(stdout);
 
-  close(sockFD);
+    uintptr_t* shared_data = (uintptr_t*)syscall(SYS_shared_memory_acquire, shared_memory_id);
+    printf("[CLIENT]: shared_data %p\n", shared_data);
 
-  printf("[CLIENT]:  closed client socket\n");
-  fflush(stdout);
+    *shared_data = 69;
 
-  return 0;
+    write(sockFD, data, 16);
+
+    printf("[CLIENT]:  client written\n");
+    fflush(stdout);
+
+    printf("[CLIENT]:  closing client socket\n");
+    fflush(stdout);
+
+    close(sockFD);
+
+    printf("[CLIENT]:  closed client socket\n");
+    fflush(stdout);
+
+    return 0;
 }
--- user/servertest/main.c
+++ user/servertest/main.c
@@ -1,97 +1,105 @@
-#include <sys/syscalls.h>
-
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/syscalls.h>
 #include <unistd.h>
 
 #define SOCK_ADDR_MAX_SIZE 16
 
-int main(int argc, char **argv) {
-  printf("[SERVE]: starting...\n");
-  fflush(stdout);
-  int sockFD = sys_socket(0, 0, 0);
-
-  sockaddr addr;
-  addr.type = LENSOR16;
-  const char socket_path[] = "!Test";
-  memset(addr.data, 0, SOCK_ADDR_MAX_SIZE);
-  memcpy(addr.data, &socket_path, sizeof(socket_path) - 1);
-  // bind (set our address)
-  sys_bind(sockFD, &addr, sizeof(sockaddr));
-  // listen (mark self as server)
-  sys_listen(sockFD, 32);
-
-  // Open an event queue to be notified when an incoming connection is
-  // coming in on the server socket. This isn't really needed; we could
-  // use the blocking mechanism of accept() for this. However, with this
-  // technique, it would theoretically be possible to be doing other
-  // things first (like handling all current connections with another
-  // event queue; we'll get there) before checking if any incoming
-  // connections have come in.
-  int listen_queue = sys_kqueue();
-
-  const size_t changelist_size = 4;
-  Event changelist[changelist_size];
-  memset(changelist, 0, sizeof(changelist));
-  changelist[0].Type = EVENTTYPE_READY_TO_READ;
-  changelist[0].Filter.ProcessFD = sockFD;
-  sys_kevent(listen_queue, changelist, 1, NULL, 0);
-
-  printf("[SERVE]: Waiting for a READY_TO_READ event to come in on the listen_queue...\n");
-  fflush(stdout);
-  const size_t eventlist_size = 4;
-  Event eventlist[eventlist_size];
-  memset(eventlist, 0, sizeof(eventlist));
-  int status = 0;
-  while ((status = sys_kevent(listen_queue, NULL, 0, eventlist, eventlist_size)) != 0)
-    ;
-
-  printf("[SERVE]: Waiting for a connection to come in...\n");
-  sockaddr connected_addr;
-  size_t connected_addrlen = sizeof(sockaddr);
-  int clientFD = -1;
-  // Attempt to accept incoming connection. If given the retry return code,
-  // retry.
-  do {
-    printf("[SERVE]: Accepting...\n");
+int main(int argc, char** argv) {
+    printf("[SERVE]: starting...\n");
     fflush(stdout);
-    // We will block here until a connection is made.
-    clientFD = sys_accept(sockFD, &connected_addr, &connected_addrlen);
-    printf("[SERVE]: accept returned %d\n", clientFD);
-    fflush(stdout);
-  }
-  while (clientFD == -2)
-    ;
+    int sockFD = sys_socket(0, 0, 0);
 
-  if (clientFD < 0) {
+    sockaddr addr;
+    addr.type = LENSOR16;
+    const char socket_path[] = "!Test";
+    memset(addr.data, 0, SOCK_ADDR_MAX_SIZE);
+    memcpy(addr.data, &socket_path, sizeof(socket_path) - 1);
+    // bind (set our address)
+    sys_bind(sockFD, &addr, sizeof(sockaddr));
+    // listen (mark self as server)
+    sys_listen(sockFD, 32);
+
+    // Open an event queue to be notified when an incoming connection is
+    // coming in on the server socket. This isn't really needed; we could
+    // use the blocking mechanism of accept() for this. However, with this
+    // technique, it would theoretically be possible to be doing other
+    // things first (like handling all current connections with another
+    // event queue; we'll get there) before checking if any incoming
+    // connections have come in.
+    int listen_queue = sys_kqueue();
+
+    const size_t changelist_size = 4;
+    Event changelist[changelist_size];
+    memset(changelist, 0, sizeof(changelist));
+    changelist[0].Type = EVENTTYPE_READY_TO_READ;
+    changelist[0].Filter.ProcessFD = sockFD;
+    // This applies the above change to the event queue, meaning we will
+    // recieve events when the given file descriptor is ready to read from.
+    // In the case of a local socket, that means a process has connected.
+    sys_kevent(listen_queue, changelist, 1, NULL, 0);
+
+    printf("[SERVE]: Waiting for a READY_TO_READ event to come in on the listen_queue...\n");
+    fflush(stdout);
+    const size_t eventlist_size = 4;
+    Event eventlist[eventlist_size];
+    memset(eventlist, 0, sizeof(eventlist));
+    int status = 0;
+    // FIXME: This waits for there not to be an error; technically, we should
+    // either block until there is an event in the syscall itself, or wait
+    // until some sign that events have been written on the userspace side of
+    // things.
+    while ((status = sys_kevent(listen_queue, NULL, 0, eventlist, eventlist_size)) != 0);
+
+    printf("[SERVE]: Waiting for a connection to come in...\n");
+    sockaddr connected_addr;
+    size_t connected_addrlen = sizeof(sockaddr);
+    int clientFD = -1;
+    // Attempt to accept incoming connection. If given the retry return code,
+    // retry.
+    do {
+        printf("[SERVE]: Accepting...\n");
+        fflush(stdout);
+        // We will block here until a connection is made.
+        clientFD = sys_accept(sockFD, &connected_addr, &connected_addrlen);
+        printf("[SERVE]: accept returned %d\n", clientFD);
+        fflush(stdout);
+    } while (clientFD == -2);
+
+    if (clientFD < 0) {
+        close(sockFD);
+        printf("[SERVE]: `accept` failed: %d\n", clientFD);
+        return 1;
+    }
+    printf("[SERVE]: Connection accepted: clientFD=%d\n", clientFD);
+
+    uintptr_t* shared_data = NULL;
+    int id = syscall(SYS_shared_memory_allocate, &shared_data, 4096);
+    printf("id:%d data:%p\n", id, shared_data);
+
+    uintptr_t payload[3] = {69, 420, id};
+
+    printf("[SERVE]: writing...\n");
+    fflush(stdout);
+
+    write(clientFD, payload, sizeof(payload));
+
+    unsigned char data[512];
+    int bytes_read = 0;
+
+    printf("[SERVE]: reading...\n");
+    fflush(stdout);
+    bytes_read += read(clientFD, data, 512);
+
+    printf("[SERVE]: read %d bytes from socket\n", bytes_read);
+    printf("[SERVE]: first 8-byte of shared data: %u\n", *shared_data);
+    fflush(stdout);
+
+    close(clientFD);
+
+    printf("[SERVE]: shutting down\n");
     close(sockFD);
-    printf("[SERVE]: `accept` failed: %d\n", clientFD);
-    return 1;
-  }
-  printf("[SERVE]: Connection accepted: clientFD=%d\n", clientFD);
 
-  uint64_t payload[2] = {69, 420};
-
-  printf("[SERVE]: writing...\n");
-  fflush(stdout);
-
-  write(clientFD, payload, sizeof(payload));
-
-  unsigned char data[512];
-  int bytes_read = 0;
-
-  printf("[SERVE]: reading...\n");
-  fflush(stdout);
-  bytes_read += read(clientFD, data, 512);
-
-  printf("[SERVE]: read %d bytes from socket\n", bytes_read);
-  fflush(stdout);
-
-  close(clientFD);
-
-  printf("[SERVE]: shutting down\n");
-  close(sockFD);
-
-  return 0;
+    return 0;
 }
