78bc68a63a6fa4500580d00d91dc10d8fc079dab
Lens_r <lensplaysgames@gmail.com>
2026-09-09 22:22:15 -0700

    [Minor] Formatting

--- kernel/src/gdt.cpp
+++ kernel/src/gdt.cpp
@@ -23,6 +23,7 @@ GDT gGDT;
 GDTDescriptor gGDTD;
 
 void setup_gdt() {
+    // clang-format off
     //                  BASE  LIMIT       ACCESS       FLAGS:LIMIT1
     gGDT.Null =      {  0,    0,          0x00,        0x00        };
     gGDT.Ring0Code = {  0,    0xffffffff, 0b10011010,  0b10110000  };
@@ -30,4 +31,5 @@ void setup_gdt() {
     gGDT.Ring3Code = {  0,    0xffffffff, 0b11111010,  0b10110000  };
     gGDT.Ring3Data = {  0,    0xffffffff, 0b11110010,  0b10110000  };
     gGDT.TSS =       {{ 0,    0xffffffff, 0b10001001,  0b00100000 }};
+    // clang-format on
 }
