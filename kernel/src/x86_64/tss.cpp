9779488b8f00620b21203f5c54a1e109951e6b38
Lens_r <lensplaysgames@gmail.com>
2026-09-11 15:21:21 -0700

    [TSS] Addressing Virtual Addressing of Frame Pointers

--- kernel/src/x86_64/tss.cpp
+++ kernel/src/x86_64/tss.cpp
@@ -37,10 +37,10 @@ void initialize() {
     // Zero out TSS entry.
     memset(&tssEntry, 0, sizeof(TSSEntry));
     // Set byte limit of TSS Entry past base address.
-    u64 limit = sizeof(TSSEntry) - 1;
+    auto limit = sizeof(TSSEntry) - 1;
     gGDT.TSS.set_limit(limit);
     // Set base address to address of TSS Entry.
-    u64 base = V2P((u64)&tssEntry);
+    auto base = uintptr_t(&tssEntry);
     gGDT.TSS.set_base(base);
     std::print(
         "[TSS]: Initialized\n"
@@ -50,7 +50,7 @@ void initialize() {
         gGDT.TSS.limit());
     // 0x28 -> offset of TSS in GDT.
     asm volatile(
-        "mov $0x28, %%ax\n\t"
+        "mov $0x28, %%eax\n\t"
         "ltr %%ax\n\t" ::: "rax");
 
     // Allocate an interrupt kernel stack
@@ -70,7 +70,7 @@ void initialize() {
 
     Memory::map_pages(
         (void*)virtual_stack_base,
-        physical_stack_base,
+        (void*)Memory::TO_FRAME_POINTER(physical_stack_base),
         interrupt_stack_flags,
         InterruptStackSizePages);
 
@@ -107,7 +107,7 @@ void initialize() {
         const uintptr_t virtual_base = Memory::KERNEL_INTERRUPT_STACK_BASE + virtual_offset;
         Memory::map_pages(
             (void*)virtual_base,
-            physical_base,
+            (void*)Memory::TO_FRAME_POINTER(physical_base),
             interrupt_stack_flags,
             InterruptStackSizePages);
         uintptr_t virtual_top = virtual_base + InterruptStackSize;
