a436b9b8b0f5829d25e97f7992a7261a045a2084
Lens_r <lensplaysgames@gmail.com>
2026-09-11 15:18:26 -0700

    [GDT] Just in case...
    Use a 32-bit move in x86_64, which guarantees clearing the upper bits
    to zero. `ltr` only uses the bottom 32 bits anyway, but, if we just
    move into `ax`, the top 16 bits could contain garbage. Technically,
    the CPU isn't supposed to use the top 16 bits at all, but, I don't
    like taking risks with hardware not exactly following spec

--- kernel/src/gdt.asm
+++ kernel/src/gdt.asm
@@ -20,14 +20,14 @@
 
 LoadGDT:
     lgdt [rdi]
-    mov ax, 0x10
+    mov eax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
     pop rdi
-    mov rax, 0x08
+    mov eax, 0x08
     push rax
     push rdi
     retfq
