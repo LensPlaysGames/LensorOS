23a54103527baae9828fcb5cc16cb6495b373ba7
Lens_r <lensplaysgames@gmail.com>
2026-09-17 22:50:36 -0700

    [LICENSE]

--- kernel/src/x86_64/lapic.h
+++ kernel/src/x86_64/lapic.h
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with LensorOS. If not, see <https://www.gnu.org/licenses
+ * along with LensorOS. If not, see <https://www.gnu.org/licenses/>
  */
 
 #ifndef LENSOR_OS_LAPIC_H
