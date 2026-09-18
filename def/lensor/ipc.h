c7b9f9b3b448bc965b840e0f493999e6477991dd
Lens_r <lensplaysgames@gmail.com>
2026-09-17 21:00:10 -0700

    [IPC]

--- def/lensor/ipc.h
+++ def/lensor/ipc.h
@@ -3,12 +3,15 @@
 
 #include <stdint.h>
 
+#define IPC_MAX_SIZE 32
+
 #define IPC_KEYBOARD_MAGIC 0xf8
 typedef struct ipc_keyboard_t {
     uint8_t magic;
     uint8_t is_pressed;
     uint16_t value;
 } ipc_keyboard_t;
+static_assert(sizeof(ipc_keyboard_t) <= IPC_MAX_SIZE);
 
 #define IPC_MOUSE_POSITION_MAGIC 0xf9
 typedef struct ipc_mouse_position_t {
@@ -16,6 +19,7 @@ typedef struct ipc_mouse_position_t {
     int32_t local_x;
     int32_t local_y;
 } ipc_mouse_position_t;
+static_assert(sizeof(ipc_mouse_position_t) <= IPC_MAX_SIZE);
 
 #define IPC_MOUSE_DELTA_MAGIC 0xfa
 typedef struct ipc_mouse_delta_t {
@@ -23,5 +27,6 @@ typedef struct ipc_mouse_delta_t {
     int32_t delta_x;
     int32_t delta_y;
 } ipc_mouse_delta_t;
+static_assert(sizeof(ipc_mouse_delta_t) <= IPC_MAX_SIZE);
 
 #endif /* LENSOROS_DEFINES_IPC_H */
